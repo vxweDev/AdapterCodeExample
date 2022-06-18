c24637b8359c965be5ddbb8be04ad7f876daebda
Florian <florian.thuswohl@aon.at>
2022-06-18 21:21:51 +0200

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,37 @@
+import adapters.SquarePegAdapter;
+import round.RoundHole;
+import round.RoundPeg;
+import square.SquarePeg;
+
+
+public class Test {
+    public static void main(String[] args) {
+        int radHole = 5;
+        int radPeg = 5;
+
+        // round fits round
+        RoundHole hole = new RoundHole(radHole);
+        RoundPeg rpeg = new RoundPeg(radPeg);
+        if (hole.fits(rpeg)) {
+            System.out.println("Round peg radius 5 fits round hole radius 5.");
+        }
+
+
+        int widthSmall = 2;
+        int widthLarge = 20;
+
+        SquarePeg smallSqPeg = new SquarePeg(widthSmall);
+        SquarePeg largeSqPeg = new SquarePeg(widthLarge);
+        // hole.fits(smallSqPeg); // Won't compile → square is not round
+
+        // Adapter can solve the problem
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg width" + widthSmall + " fits round hole radius " + radHole);
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg width" + widthLarge+ " does not fit round hole radius " + radHole);
+        }
+    }
+}
